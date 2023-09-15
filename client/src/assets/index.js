f5f3d21655951cb20cb0ac526229b6ed5ef72b30
Leo-2807 <82811990+Leo-2807@users.noreply.github.com>
2023-09-15 09:23:09 +0530

    Initial commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,6 @@
+export { AddChannel } from './AddChannel.js';
+export { ChannelInfo } from './ChannelInfo.js';
+export { CloseCreateChannel } from './CloseCreateChannel.js';
+export { InviteIcon } from './InviteIcon.js';
+export { LightningBolt } from './LightningBolt.js';
+export { SearchIcon } from './SearchIcon.js';
